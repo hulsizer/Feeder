9fe49f20e2275627d798499e9e90cc188c8bcf6f
Andrew Hulsizer <andrew.hulsizer@gmail.com>
2013-04-14 09:01:11 -0500

    Initial Commit

---
+++ Feeder/CLMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLMAppDelegate.h
+//  Feeder
+//
+//  Created by Andrew Hulsizer on 4/14/13.
+//  Copyright (c) 2013 Andrew Hulsizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Feeder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Feeder
+//
+//  Created by Andrew Hulsizer on 4/14/13.
+//  Copyright (c) 2013 Andrew Hulsizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CLMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLMAppDelegate class]));
+    }
+}
