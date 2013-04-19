b8f46cb363a5650145fefc7ea5d848bc950ca17b
Andrew Hulsizer <andrew.hulsizer@gmail.com>
2013-04-18 21:18:50 -0500

    Hack together an animation for the login screen

--- Feeder/Classes/ViewControllers/Login/CLMLoginViewController.m
+++ Feeder/Classes/ViewControllers/Login/CLMLoginViewController.m
@@ -10,7 +10,14 @@
 #import <QuartzCore/QuartzCore.h>
 @interface CLMLoginViewController ()
 
-@property (nonatomic, strong) IBOutlet UIView *imageView;
+@property (nonatomic, strong) IBOutlet UILabel *selectLabel;
+@property (nonatomic, strong) IBOutlet UIButton *cancelButton;
+@property (nonatomic, strong) IBOutlet UIButton *loginButton;
+@property (nonatomic, strong) IBOutlet UIButton *googleButton;
+@property (nonatomic, strong) IBOutlet UIButton *facebookButton;
+@property (nonatomic, strong) IBOutlet UIButton *twitterButton;
+@property (nonatomic, strong) IBOutlet UIImageView *dailyImage;
+@property (nonatomic, strong) IBOutlet UIView *loginView;
 @property (nonatomic, strong) CAShapeLayer *maskLayer;
 @property (nonatomic, assign) BOOL isMaskOpen;
 
@@ -83,30 +90,108 @@
 	[self.maskLayer setPath:firstPath];
 	[self.maskLayer setFillRule:kCAFillRuleEvenOdd];
 	
-	self.imageView.layer.mask = self.maskLayer;
+	self.loginView.layer.mask = self.maskLayer;
 	
+    [self blurImage];
+}
+
+- (void)blurImage
+{
+    
+    //create our blurred image
+    CIContext *context = [CIContext contextWithOptions:nil];
+    CIImage *inputImage = [CIImage imageWithCGImage:self.dailyImage.image.CGImage];
+    
+    //setting up Gaussian Blur (we could use one of many filters offered by Core Image)
+    CIFilter *filter = [CIFilter filterWithName:@"CIGaussianBlur"];
+    [filter setValue:inputImage forKey:kCIInputImageKey];
+    [filter setValue:[NSNumber numberWithFloat:5.0f] forKey:@"inputRadius"];
+    CIImage *result = [filter valueForKey:kCIOutputImageKey];
+    //CIGaussianBlur has a tendency to shrink the image a little, this ensures it matches up exactly to the bounds of our original image
+    CGImageRef cgImage = [context createCGImage:result fromRect:[inputImage extent]];
+    
+    //add our blurred image to the scrollview
+    self.dailyImage.image = [UIImage imageWithCGImage:cgImage];
 }
 
 - (IBAction)login:(id)sender
 {
-	CGPathRef path;
 	if (self.isMaskOpen)
 	{
-		path = [self makeHexagonPathWithRadius:0];
-		self.isMaskOpen = NO;
+		[self closeMask];
+        self.isMaskOpen = NO;
 	}else{
-		path = [self makeHexagonPathWithRadius:100];
-		self.isMaskOpen = YES;
+		[self openMask];
+        self.isMaskOpen = YES;
 	}
+}
+
+- (void)openMask
+{
+    CGPathRef path = [self makeHexagonPathWithRadius:90];
 	
 	CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"path"];
 	[anim setFromValue:(__bridge id)(self.maskLayer.path)];
 	[anim setToValue:(__bridge id)(path)];
 	[anim setDelegate:self];
-	[anim setDuration:.5];
+	[anim setDuration:1];
 	[anim setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
 	self.maskLayer.path = path;
 	[self.maskLayer addAnimation:anim forKey:@"path"];
+    
+    [UIView animateWithDuration:.25 animations:^{
+        [self.loginButton setAlpha:0.0];
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:.5 animations:^{
+            [self.cancelButton setAlpha:1.0];
+            [self.selectLabel setAlpha:1.0];
+        }];
+    }];
+
+}
+
+- (void)closeMask
+{
+    [UIView animateWithDuration:.5 animations:^{
+            [self.googleButton setCenter:CGPointMake(89, 158)];
+            [self.facebookButton setCenter:CGPointMake(278, 272)];
+            [self.twitterButton setCenter:CGPointMake(89, 382)];
+    } completion:^(BOOL finished) {
+        CGPathRef path = [self makeHexagonPathWithRadius:0];
+        
+        CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"path"];
+        [anim setFromValue:(__bridge id)(self.maskLayer.path)];
+        [anim setToValue:(__bridge id)(path)];
+        [anim setDelegate:self];
+        [anim setDuration:1];
+        [anim setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
+        self.maskLayer.path = path;
+        [self.maskLayer addAnimation:anim forKey:@"path"];
+    }];
+    
+    [UIView animateWithDuration:.25 animations:^{
+        [self.cancelButton setAlpha:0.0];
+        [self.selectLabel setAlpha:0.0];
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:.5 animations:^{
+            [self.loginButton setAlpha:1.0];
+        }];
+    }];
+}
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag
+{
+    if (flag)
+    {
+        [UIView animateWithDuration:.5 animations:^{
+            if (self.isMaskOpen)
+            {
+                [self.googleButton setCenter:CGPointMake(137, 241)];
+                [self.facebookButton setCenter:CGPointMake(195, 272)];
+                [self.twitterButton setCenter:CGPointMake(137, 306)];
+                
+            }
+        }];
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -114,5 +199,38 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - CAAnimations
+
+-(CABasicAnimation *)bounceAnimationFrom:(NSValue *)from
+                                      to:(NSValue *)to
+                              forKeyPath:(NSString *)keypath
+                            withDuration:(CFTimeInterval)duration
+{
+    CABasicAnimation * result = [CABasicAnimation animationWithKeyPath:keypath];
+    [result setFromValue:from];
+    [result setToValue:to];
+    [result setDuration:duration];
+    
+    [result setTimingFunction:[CAMediaTimingFunction functionWithControlPoints:.5 :1.8 :.8 :0.8]];
+    
+    return  result;
+}
+
+
+-(CAKeyframeAnimation *)keyframeBounceAnimationFrom:(NSValue *)from
+                                                via:(NSValue *)via
+                                                 to:(NSValue *)to
+                                         forKeypath:(NSString *)keyPath
+                                       withDuration:(CFTimeInterval)duration
+{
+    CAKeyframeAnimation * animation = [CAKeyframeAnimation animationWithKeyPath:keyPath];
+    [animation setValues:[NSArray arrayWithObjects:from, via, to, nil]];
+    [animation setKeyTimes:[NSArray arrayWithObjects:[NSNumber numberWithFloat:0.0], [NSNumber numberWithFloat:.7], [NSNumber numberWithFloat:1.0], nil]];
+    
+    animation.duration = duration;
+    return animation;
+}
+
 
 @end
